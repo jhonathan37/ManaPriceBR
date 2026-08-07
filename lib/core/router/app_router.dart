39e56c2f84cbd031cede434980b639734f4d494d
jhonathan37 <TWO_VR@HOTMAIL.COM>
2026-08-07 11:39:57 -0300

    feat: connect main app navigation

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
 
+import '../../presentation/batch/batch_page.dart';
 import '../../presentation/home/home_page.dart';
+import '../../presentation/search/search_page.dart';
+import '../../presentation/settings/settings_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
@@ -8,6 +11,18 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: '/search',
+      builder: (context, state) => const SearchPage(),
+    ),
+    GoRoute(
+      path: '/batch',
+      builder: (context, state) => const BatchPage(),
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (context, state) => const SettingsPage(),
     ),
   ],
 );
