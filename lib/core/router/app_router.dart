8ffa239b239c50d2cfedc041f16a1bab0cc941c9
jhonathan37 <TWO_VR@HOTMAIL.COM>
2026-08-07 11:44:07 -0300

    feat: connect scanner and result routes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,27 +2,24 @@ import 'package:go_router/go_router.dart';
 
 import '../../presentation/batch/batch_page.dart';
 import '../../presentation/home/home_page.dart';
+import '../../presentation/result/result_page.dart';
+import '../../presentation/scanner/scanner_page.dart';
 import '../../presentation/search/search_page.dart';
 import '../../presentation/settings/settings_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomePage()),
+    GoRoute(path: '/search', builder: (context, state) => const SearchPage()),
+    GoRoute(path: '/batch', builder: (context, state) => const BatchPage()),
+    GoRoute(path: '/settings', builder: (context, state) => const SettingsPage()),
+    GoRoute(path: '/scanner', builder: (context, state) => const ScannerPage()),
     GoRoute(
-      path: '/',
-      builder: (context, state) => const HomePage(),
-    ),
-    GoRoute(
-      path: '/search',
-      builder: (context, state) => const SearchPage(),
-    ),
-    GoRoute(
-      path: '/batch',
-      builder: (context, state) => const BatchPage(),
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (context, state) => const SettingsPage(),
+      path: '/result',
+      builder: (context, state) => ResultPage(
+        cardName: (state.extra as Map<String, dynamic>?)?['name'] as String?,
+      ),
     ),
   ],
 );
