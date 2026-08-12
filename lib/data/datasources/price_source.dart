e879bc8e045579835c9227a3eb8db071e5274f29
jhonathan37 <TWO_VR@HOTMAIL.COM>
2026-08-12 11:40:51 -0300

    feat: require exact printing fields for price lookup

--- lib/data/datasources/price_source.dart
+++ lib/data/datasources/price_source.dart
@@ -1,15 +1,25 @@
 class PriceLookupRequest {
   const PriceLookupRequest({
     required this.cardName,
+    this.setCode,
+    this.setName,
+    this.collectorNumber,
     this.language = 'Português',
     this.condition = 'NM',
     this.foil = false,
   });
 
   final String cardName;
+  final String? setCode;
+  final String? setName;
+  final String? collectorNumber;
   final String language;
   final String condition;
   final bool foil;
+
+  bool get hasExactPrinting =>
+      setCode != null && setCode!.isNotEmpty &&
+      collectorNumber != null && collectorNumber!.isNotEmpty;
 }
 
 class PriceLookupResponse {
